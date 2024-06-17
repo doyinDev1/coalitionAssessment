00bb9af4a647dad4e90037a0eda47ba97d8c0841
doyinDev1 <ade.doyin27@gmail.com>
2024-06-17 19:21:44 +0100

    fix cards

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -5,7 +5,12 @@ import moreHorizontal from '../../assets/MOREHORIZONTAL.png'
 import user from '../../assets/user.png'
 import girl from '../../assets/girl.png'
 import birth from '../../assets/birth.png'
-
+import download from '../../assets/download.png'
+import respiratory from '../../assets/respiratory.png'
+import temperature from '../../assets/temperature.png'
+import heart from '../../assets/Heart.png'
+import {ReactComponent as ArrowDown} from '../../assets/ArrowDown.svg'
+import {ReactComponent as ArrowUp} from '../../assets/ArrowUp.svg'
 const Content = () => {
     return (
         <div className={classes.content}>
@@ -16,7 +21,36 @@ const Content = () => {
                     </h1>
                     <img src={search} alt='' />
                 </div>
+
                 <div className={classes.leftMain}>
+                    <div className={`${classes.MainWrap} ${classes.activePatient}`}>
+                        <div className={classes.userMain}>
+                            <img src={user} alt='' />
+                            <div className={classes.users}>
+                                <h1>
+                                    Emily Williams
+                                </h1>
+                                <h2>
+                                    Female, 18
+                                </h2>
+                            </div>
+                        </div>
+                        <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
+                    </div>
+                    <div className={classes.MainWrap}>
+                        <div className={classes.userMain}>
+                            <img src={user} alt='' />
+                            <div className={classes.users}>
+                                <h1>
+                                    Emily Williams
+                                </h1>
+                                <h2>
+                                    Female, 18
+                                </h2>
+                            </div>
+                        </div>
+                        <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
+                    </div>
                     <div className={classes.MainWrap}>
                         <div className={classes.userMain}>
                             <img src={user} alt='' />
@@ -45,6 +79,91 @@ const Content = () => {
                         </div>
                         <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
                     </div>
+                    <div className={classes.MainWrap}>
+                        <div className={classes.userMain}>
+                            <img src={user} alt='' />
+                            <div className={classes.users}>
+                                <h1>
+                                    Emily Williams
+                                </h1>
+                                <h2>
+                                    Female, 18
+                                </h2>
+                            </div>
+                        </div>
+                        <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
+                    </div>
+                    <div className={classes.MainWrap}>
+                        <div className={classes.userMain}>
+                            <img src={user} alt='' />
+                            <div className={classes.users}>
+                                <h1>
+                                    Emily Williams
+                                </h1>
+                                <h2>
+                                    Female, 18
+                                </h2>
+                            </div>
+                        </div>
+                        <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
+                    </div>
+                    <div className={classes.MainWrap}>
+                        <div className={classes.userMain}>
+                            <img src={user} alt='' />
+                            <div className={classes.users}>
+                                <h1>
+                                    Emily Williams
+                                </h1>
+                                <h2>
+                                    Female, 18
+                                </h2>
+                            </div>
+                        </div>
+                        <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
+                    </div>
+                    <div className={classes.MainWrap}>
+                        <div className={classes.userMain}>
+                            <img src={user} alt='' />
+                            <div className={classes.users}>
+                                <h1>
+                                    Emily Williams
+                                </h1>
+                                <h2>
+                                    Female, 18
+                                </h2>
+                            </div>
+                        </div>
+                        <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
+                    </div>
+                    <div className={classes.MainWrap}>
+                        <div className={classes.userMain}>
+                            <img src={user} alt='' />
+                            <div className={classes.users}>
+                                <h1>
+                                    Emily Williams
+                                </h1>
+                                <h2>
+                                    Female, 18
+                                </h2>
+                            </div>
+                        </div>
+                        <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
+                    </div>
+                    <div className={classes.MainWrap}>
+                        <div className={classes.userMain}>
+                            <img src={user} alt='' />
+                            <div className={classes.users}>
+                                <h1>
+                                    Emily Williams
+                                </h1>
+                                <h2>
+                                    Female, 18
+                                </h2>
+                            </div>
+                        </div>
+                        <img src={moreHorizontal} className={classes.optionsHorizon} alt='' />
+                    </div>
+
                     <div className={classes.MainWrap}>
                         <div className={classes.userMain}>
                             <img src={user} alt='' />
@@ -103,85 +222,222 @@ const Content = () => {
                     </div>
                 </div>
             </div>
+
+
+
+
             <div className={classes.mid}>
-                2
+                <div className={classes.midTop}>
+                    <h1 className={classes.midTopText}>
+                        Diagnosis History
+                    </h1>
+                    <div className={classes.historyBottom}>
+                        <div className={classes.colouredCard} style={{ backgroundColor: '#E0F3FA' }}>
+                            <img src={respiratory} alt='' />
+                            <h1>Respiratory Rate</h1>
+                            <h2>20 bpm</h2>
+                            <h3>Normal</h3>
+                        </div>
+                        <div className={classes.colouredCard} style={{ backgroundColor: '#FFE6E9' }}>
+                            <img src={temperature} alt='' />
+                            <h1>Temperature</h1>
+                            <h2>98.6°F</h2>
+                            <h3>Normal</h3>
+                        </div>
+                        <div className={classes.colouredCard} style={{ backgroundColor: '#FFE6F1' }}>
+                            <img src={heart} alt='' />
+                            <h1>Heart Rate</h1>
+                            <h2>78 bpm</h2>
+                            <div>
+
+                            </div>
+                            <h3>Lower than Average</h3>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={classes.midBottom}>
+                    <div className={classes.bottomHeader}>
+                        <h1>
+                            Diagnostic List
+                        </h1>
+                    </div>
+                    <div className={classes.bottomTable}>
+                        <table>
+
+                            <thead>
+                                <tr>
+                                    <th>Problem</th>
+                                    <th>Description</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <div style={{ paddingTop: '31px' }} />
+                            <tbody>
+                                <tr>
+                                    <td>Login Issue</td>
+                                    <td>Users unable to login with correct credentials</td>
+                                    <td>Open</td>
+                                </tr>
+                                <tr>
+                                    <td>Payment Error</td>
+                                    <td>Transactions fail for certain credit cards</td>
+                                    <td>In Progress</td>
+                                </tr>
+                                <tr>
+                                    <td>Page Load</td>
+                                    <td>Homepage takes too long to load</td>
+                                    <td>Resolved</td>
+                                </tr>
+                                <tr>
+                                    <td>Page Load</td>
+                                    <td>Homepage takes too long to load</td>
+                                    <td>Resolved</td>
+                                </tr>
+                                <tr>
+                                    <td>Page Load</td>
+                                    <td>Homepage takes too long to load</td>
+                                    <td>Resolved</td>
+                                </tr>
+                                <tr>
+                                    <td>Page Load</td>
+                                    <td>Homepage takes too long to load</td>
+                                    <td>Resolved</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+
+                </div>
             </div>
+
+
+
+
             <div className={classes.right}>
                 <div className={classes.rightMain} >
-                <div className={classes.rightTop}>
-                    <img src={girl} alt='' />
-                    <h1>Jessica Taylor</h1>
-                </div>
-                <div className={classes.userDetails}>
-                    <div className={classes.userInfo}>
-                        <img src={birth} alt='' />
+                    <div className={classes.rightTop}>
+                        <img src={girl} alt='' />
+                        <h1>Jessica Taylor</h1>
+                    </div>
+                    <div className={classes.userDetails}>
+                        <div className={classes.userInfo}>
+                            <img src={birth} alt='' />
 
-                        <div className={classes.userText}>
-                            <h1>
-                                Date of birth
-                            </h1>
-                            <h2>
-                                Aug 26th, 1999
-                            </h2>
+                            <div className={classes.userText}>
+                                <h1>
+                                    Date of birth
+                                </h1>
+                                <h2>
+                                    Aug 26th, 1999
+                                </h2>
+                            </div>
+                        </div>
+                        <div className={classes.userInfo}>
+                            <img src={birth} alt='' />
+
+                            <div className={classes.userText}>
+                                <h1>
+                                    Date of birth
+                                </h1>
+                                <h2>
+                                    Aug 26th, 1999
+                                </h2>
+                            </div>
+                        </div>
+                        <div className={classes.userInfo}>
+                            <img src={birth} alt='' />
+
+                            <div className={classes.userText}>
+                                <h1>
+                                    Date of birth
+                                </h1>
+                                <h2>
+                                    Aug 26th, 1999
+                                </h2>
+                            </div>
+                        </div>
+                        <div className={classes.userInfo}>
+                            <img src={birth} alt='' />
+
+                            <div className={classes.userText}>
+                                <h1>
+                                    Date of birth
+                                </h1>
+                                <h2>
+                                    Aug 26th, 1999
+                                </h2>
+                            </div>
+                        </div>
+                        <div className={classes.userInfo}>
+                            <img src={birth} alt='' />
+
+                            <div className={classes.userText}>
+                                <h1>
+                                    Date of birth
+                                </h1>
+                                <h2>
+                                    Aug 26th, 1999
+                                </h2>
+                            </div>
+                        </div>
+                        <div className={classes.btn}>
+                            <button>Show All Information</button>
                         </div>
                     </div>
-                    <div className={classes.userInfo}>
-                        <img src={birth} alt='' />
-
-                        <div className={classes.userText}>
-                            <h1>
-                                Date of birth
-                            </h1>
-                            <h2>
-                                Aug 26th, 1999
-                            </h2>
-                        </div>
-                    </div>
-                    <div className={classes.userInfo}>
-                        <img src={birth} alt='' />
-
-                        <div className={classes.userText}>
-                            <h1>
-                                Date of birth
-                            </h1>
-                            <h2>
-                                Aug 26th, 1999
-                            </h2>
-                        </div>
-                    </div>
-                    <div className={classes.userInfo}>
-                        <img src={birth} alt='' />
-
-                        <div className={classes.userText}>
-                            <h1>
-                                Date of birth
-                            </h1>
-                            <h2>
-                                Aug 26th, 1999
-                            </h2>
-                        </div>
-                    </div>
-                    <div className={classes.userInfo}>
-                        <img src={birth} alt='' />
-
-                        <div className={classes.userText}>
-                            <h1>
-                                Date of birth
-                            </h1>
-                            <h2>
-                                Aug 26th, 1999
-                            </h2>
-                        </div>
-                    </div>
-                    <div className={classes.btn}>
-                        <button>Show All Information</button>
-                    </div>
-                </div>
 
                 </div>
                 <div className={classes.rightBottom}>
 
-                fdfdfdfdf
+                    <h1>
+                        Lab Results
+                    </h1>
+                    <div className={classes.testWrap}>
+                        <div className={classes.testCases}>
+                            <h2>
+                                Urine Test
+                            </h2>
+                            <img src={download} alt='' />
+                        </div>
+                        <div className={`${classes.testCases} ${classes.active}`}>
+                            <h2>
+                                Urine Test
+                            </h2>
+                            <img src={download} alt='' />
+                        </div>
+                        <div className={classes.testCases}>
+                            <h2>
+                                Urine Test
+                            </h2>
+                            <img src={download} alt='' />
+                        </div>
+                        <div className={classes.testCases}>
+                            <h2>
+                                Urine Test
+                            </h2>
+                            <img src={download} alt='' />
+                        </div>
+                        <div className={classes.testCases}>
+                            <h2>
+                                Urine Test
+                            </h2>
+                            <img src={download} alt='' />
+                        </div>
+                        <div className={classes.testCases}>
+                            <h2>
+                                Urine Test
+                            </h2>
+                            <img src={download} alt='' />
+                        </div>
+                        <div className={classes.testCases}>
+                            <h2>
+                                Urine Test
+                            </h2>
+                            <img src={download} alt='' />
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
